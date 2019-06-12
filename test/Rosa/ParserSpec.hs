214ca52168338f51a3d12db062fe1e803029c423
jmalena <jonas.malena@gmail.com>
2019-06-12 19:44:08 +0200

    test more concrete AST trees in ParserSpec

--- test/Rosa/ParserSpec.hs
+++ test/Rosa/ParserSpec.hs
@@ -1,5 +1,6 @@
 module Rosa.ParserSpec where
 
+import Rosa.AST
 import Rosa.Parser
 import Rosa.Parser.Gen
 import Rosa.PrettyPrinter
@@ -8,11 +9,28 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
+prop_doubleInverse :: [Defn] -> Bool
+prop_doubleInverse ast =
+  case parse (prettyPrint ast) of
+    Left err -> error err
+    Right ast' ->
+      ast' == ast
+
 spec :: Spec
 spec = do
-  describe "Parser" $
-    prop "should restore pretty printed AST" $ forAll genProgram $ \ast ->
-      case parse (prettyPrint ast) of
-        Left err -> error err
-        Right ast' ->
-          ast' == ast
+  describe "Parser" $ do
+    prop "should restore generic pretty printed program AST" $
+      forAll (genProgram $ sequence [Return <$> genExpr]) $
+      prop_doubleInverse
+
+    prop "should restore arithmetic pretty printed program AST" $
+      forAll (genProgram $ sequence [Return <$> genArithOpsExpr64]) $
+      prop_doubleInverse
+
+    prop "should restore logic pretty printed program AST" $
+      forAll (genProgram $ sequence [Return <$>  genLogicOpsExpr64]) $
+      prop_doubleInverse
+
+    prop "should restore bitwise logic pretty printed program AST" $
+      forAll (genProgram $ sequence [Return <$> genBitLogicOpsExpr64]) $
+      prop_doubleInverse
